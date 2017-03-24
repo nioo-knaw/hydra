2638af326366c792ad3e9f9f8fa3687032d6c812
Mattias <m.dehollander@nioo.knaw.nl>
2017-03-24 14:35:01 +0100

    Correct path metadata

--- conf.py
+++ conf.py
@@ -80,7 +80,7 @@ def make_config(config, path):
     conf["reverse_primer"] = "GACTACHVGGGTATCTAATCC"
 
     conf["silva_arb"] = "/data/db/Silva/128/SSURef_NR99_128_SILVA_07_09_16_opt.arb"
-    conf["metadata"] = "data/metadata.txt"
+    conf["metadata"] = "../data/metadata.txt"
 
     conf["data"] = samples
 
